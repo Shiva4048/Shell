a8c092ad831557e95a742ff20bb2b2f5fdcca13d
Shiva4048 <sivasai.laveti@gmail.com>
2024-09-14 16:23:25 +0530

    Script Updated

---
+++ number.sh
@@ -0,0 +1,28 @@
+#1/bin/bash
+
+echo "Enter First Choice :"
+read a
+echo "Enter Second choice :"
+read b
+echo "Enter Final Choice :"
+read c
+
+if [ $a -gt $b ] && [ $a -gt $c];
+then
+echo "The Greatest Number is : $a"
+
+elif [ $b -gt $a ] && [ $b -gt $c ];
+then
+echo "The Greatest Number is : $b"
+
+elif [ $c -gt $a ] && [ $c -gt $b ]
+then
+echo "The Greatest Number is : $c"
+
+elif [ $a -eq $b ] && [ $a -eq $c ] && [ $b -eq $a ] && [ $b -eq $c ] && [ $c -eq $a ] && [ $c -eq $b ]
+then
+echo "The Greatest Number is Same"
+else
+echo "Please enter valid Input"
+
+fi
